eeb1d8a040b9510189a714216eee2a31df5a8d3b
jorozco304 <jean14.jcom@gmail.com>
2023-09-24 14:49:54 -0600

    Semana 1, creación proyecto

---
+++ src/main/java/com/Tienda_DesarrolloWeb/TiendaDesarrolloWebApplication.java
@@ -0,0 +1,13 @@
+package com.Tienda_DesarrolloWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaDesarrolloWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaDesarrolloWebApplication.class, args);
+	}
+
+}
